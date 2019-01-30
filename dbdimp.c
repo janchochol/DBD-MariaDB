janchochol/DBD-MariaDB

commit ea8685409ac67f4c3fe235f38b53c39fcfd68e0c

Fix retrieving $sth attributes after $sth->fetchall* call
Retrieving $sth attributes after $sth->fetchall* call was not possible. DBI
just threw following error:

DBD::MariaDB::st FETCH failed: statement contains no result

This change fixes problem by letting $sth handle active even after last row
was fetched. When $sth is not active then fetching $sth attributes is not
possible.

dbdimp.c
@@ -4756,7 +4756,6 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
   int av_length, av_readonly;
   MYSQL_ROW cols;
   D_imp_dbh_from_sth;
-  MYSQL* svsock= imp_dbh->pmysql;
   imp_sth_fbh_t *fbh;
   D_imp_xxh(sth);
 #if MYSQL_VERSION_ID >=SERVER_PREPARE_VERSION
@@ -4848,8 +4847,6 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
                  mysql_stmt_error(imp_sth->stmt),
                  mysql_stmt_sqlstate(imp_sth->stmt));
       }
-
-      mariadb_st_finish(sth, imp_sth);
 
       return Nullav;
     }
@@ -5094,12 +5091,6 @@ process:
         mariadb_dr_do_error(sth, mysql_errno(imp_dbh->pmysql),
                  mysql_error(imp_dbh->pmysql),
                  mysql_sqlstate(imp_dbh->pmysql));
-
-
-#if MYSQL_VERSION_ID >= MULTIPLE_RESULT_SET_VERSION
-      if (!mysql_more_results(svsock))
-#endif
-        mariadb_st_finish(sth, imp_sth);
       return Nullav;
     }
 

MANIFEST
@@ -42,6 +42,7 @@ t/40numrows.t
 t/40server_prepare.t
 t/40server_prepare_crash.t
 t/40server_prepare_error.t
+t/40sth_attr.t
 t/40types.t
 t/41bindparam.t
 t/41blobs_prepare.t

t/40sth_attr.t
@@ -1,0 +1,30 @@
+use strict;
+use warnings;
+
+use Test::More;
+use DBI;
+
+use vars qw($test_dsn $test_user $test_password);
+use lib 't', '.';
+require "lib.pl";
+
+my $dbh = DbiTestConnect($test_dsn, $test_user, $test_password, { PrintError => 0, RaiseError => 1 });
+
+plan tests => 11;
+
+ok($dbh->do("CREATE TEMPORARY TABLE t(id INT)"));
+ok($dbh->do("INSERT INTO t(id) VALUES(1)"));
+
+my $sth = $dbh->prepare("SELECT * FROM t");
+ok($sth->execute());
+ok($sth->fetchall_arrayref());
+is_deeply($sth->{NAME}, ["id"]);
+ok($sth->finish());
+
+my $sth2 = $dbh->prepare("SELECT * FROM t", { mariadb_server_prepare => 1 });
+ok($sth2->execute());
+ok($sth2->fetchall_arrayref());
+is_deeply($sth2->{NAME}, ["id"]);
+ok($sth2->finish());
+
+ok($dbh->disconnect());

commit a29d57a8dbc122c3a940d12dac914774243c1ad4

Set $sth->{Active} to off after fetching all data
This is requirement by DBI API. Without it DBI throws following warning
when $sth->finish() method is not called before issuing $dbh->disconnect():

  DBI::db=HASH(...)->disconnect invalidates 1 active statement handle (either destroy statement handles or call finish on them before disconnecting)

It happens also in the case when all data were fetched by some $sth->fetch*
method call.

DBI documentation about $sth->finish() method says:

  When all the data has been fetched from a SELECT statement, the driver
  will automatically call finish for you. So you should not call it
  explicitly except when you know that you've not fetched all the data from
  a statement handle and the handle won't be destroyed soon.

With this change, $sth->{Active} attribute cannot be (mis)used for other
DBD::MariaDB internal things. So error detection was modified to not depend
on it.

Method $sth->finish() was changed to not free last result set. So it is
still possible to retrive statement attributes even after calling method
$sth->finish().

Methods $sth->fetch* were modified to return undef without throwing errors
when trying to read data after all data were retrieved.

Member fetch_done was removed as it is not used anymore.

Member currow was changed from int to my_ulonglong as it contains current
number of row up to the value from row_num which is my_ulonglong.

Fixes: ea8685409ac67f4c3fe235f38b53c39fcfd68e0c
See: https://metacpan.org/pod/DBI#Active
See: https://metacpan.org/pod/DBI#finish

dbdimp.c
@@ -3712,7 +3712,7 @@ AV *mariadb_db_data_sources(SV *dbh, imp_dbh_t *imp_dbh, SV *attr)
   return av;
 }
 
-static bool mariadb_st_free_result_sets(SV *sth, imp_sth_t *imp_sth);
+static bool mariadb_st_free_result_sets(SV *sth, imp_sth_t *imp_sth, bool free_last);
 
 /* 
  **************************************************************************
@@ -3774,7 +3774,6 @@ mariadb_st_prepare_sv(
   imp_sth->use_server_side_prepare = imp_dbh->use_server_side_prepare;
   imp_sth->disable_fallback_for_server_prepare = imp_dbh->disable_fallback_for_server_prepare;
 
-  imp_sth->fetch_done = FALSE;
   imp_sth->done_desc = FALSE;
   imp_sth->result = NULL;
   imp_sth->currow = 0;
@@ -3844,7 +3843,7 @@ mariadb_st_prepare_sv(
      Clean-up previous result set(s) for sth to prevent
      'Commands out of sync' error 
   */
-  if (!mariadb_st_free_result_sets(sth, imp_sth))
+  if (!mariadb_st_free_result_sets(sth, imp_sth, TRUE))
     return 0;
 
   if (imp_sth->use_server_side_prepare)
@@ -3988,11 +3987,12 @@ mariadb_st_prepare_sv(
  *
  * Inputs: sth - Statement handle
  *         imp_sth - driver's private statement handle
+ *         free_last - free also the last result set
  *
  * Returns: TRUE ok
  *          FALSE error; mariadb_dr_do_error will be called
  *************************************************************************/
-static bool mariadb_st_free_result_sets(SV *sth, imp_sth_t *imp_sth)
+static bool mariadb_st_free_result_sets(SV *sth, imp_sth_t *imp_sth, bool free_last)
 {
   dTHX;
   D_imp_dbh_from_sth;
@@ -4030,7 +4030,7 @@ static bool mariadb_st_free_result_sets(SV *sth, imp_sth_t *imp_sth)
         imp_dbh->insertid = imp_sth->insertid = mysql_insert_id(imp_dbh->pmysql);
       }
     }
-    if (imp_sth->result)
+    if (imp_sth->result && (mysql_more_results(imp_dbh->pmysql) || free_last))
     {
       mysql_free_result(imp_sth->result);
       imp_sth->result=NULL;
@@ -4127,7 +4127,6 @@ bool mariadb_st_more_results(SV* sth, imp_sth_t* imp_sth)
     imp_sth->result= NULL;
   }
 
-  if (DBIc_ACTIVE(imp_sth))
     DBIc_ACTIVE_off(imp_sth);
 
   next_result_return_code= mysql_next_result(imp_dbh->pmysql);
@@ -4607,7 +4606,7 @@ IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
      Clean-up previous result set(s) for sth to prevent
      'Commands out of sync' error 
   */
-  if (!mariadb_st_free_result_sets(sth, imp_sth))
+  if (!mariadb_st_free_result_sets(sth, imp_sth, TRUE))
     return -2;
 
   if (use_server_side_prepare)
@@ -4685,7 +4684,6 @@ IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
       DBIc_ACTIVE_on(imp_sth);
       if (!use_server_side_prepare)
         imp_sth->done_desc = FALSE;
-      imp_sth->fetch_done = FALSE;
     }
   }
 
@@ -4728,6 +4726,9 @@ static int mariadb_st_describe(SV* sth, imp_sth_t* imp_sth)
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t--> mariadb_st_describe\n");
 
+  if (imp_sth->done_desc)
+    return 1;
+
   if (imp_sth->use_server_side_prepare)
   {
     int i;
@@ -4739,9 +4740,6 @@ static int mariadb_st_describe(SV* sth, imp_sth_t* imp_sth)
     if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
       PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t\tmariadb_st_describe() num_fields %d\n",
                     num_fields);
-
-    if (imp_sth->done_desc)
-      return 1;
 
     if (num_fields <= 0 || !imp_sth->result)
     {
@@ -4906,26 +4904,14 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
         return Nullav;
   }
 
-  if (imp_sth->use_server_side_prepare)
+  if (!imp_sth->result)
   {
-    if (!DBIc_ACTIVE(imp_sth) )
-    {
-      mariadb_dr_do_error(sth, CR_UNKNOWN_ERROR, "no statement executing", "HY000");
-      return Nullav;
-    }
-
-    if (imp_sth->fetch_done)
-    {
-      mariadb_dr_do_error(sth, CR_UNKNOWN_ERROR, "fetch() but fetch already done", "HY000");
-      return Nullav;
-    }
-
-    if (!imp_sth->done_desc)
-    {
-      if (!mariadb_st_describe(sth, imp_sth))
-        return Nullav;
-    }
+    mariadb_dr_do_error(sth, CR_UNKNOWN_ERROR, "fetch() without execute()", "HY000");
+    return Nullav;
   }
+
+  if (!DBIc_ACTIVE(imp_sth))
+    return Nullav;
 
   ChopBlanks = DBIc_is(imp_sth, DBIcf_ChopBlanks) ? TRUE : FALSE;
 
@@ -4934,19 +4920,16 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
                   "\t\tmariadb_st_fetch for %p, chopblanks %d\n",
                   sth, ChopBlanks ? 1 : 0);
 
-  if (!imp_sth->result)
-  {
-    mariadb_dr_do_error(sth, CR_UNKNOWN_ERROR, "fetch() without execute()", "HY000");
-    return Nullav;
-  }
-
   /* fix from 2.9008 */
   imp_dbh->pmysql->net.last_errno = 0;
 
   if (imp_sth->use_server_side_prepare)
   {
+    if (!mariadb_st_describe(sth, imp_sth))
+      return Nullav;
+
     if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
-      PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t\tmariadb_st_fetch calling mysql_fetch\n");
+      PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t\tmariadb_st_fetch calling mysql_stmt_fetch\n");
 
     if ((rc= mysql_stmt_fetch(imp_sth->stmt)))
     {
@@ -4962,7 +4945,6 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
       {
         /* Update row_num to affected_rows value */
         imp_sth->row_num= mysql_stmt_affected_rows(imp_sth->stmt);
-        imp_sth->fetch_done = TRUE;
         if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
           PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t\tmariadb_st_fetch no data\n");
       }
@@ -4973,11 +4955,16 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
                  mysql_stmt_sqlstate(imp_sth->stmt));
       }
 
+      DBIc_ACTIVE_off(imp_sth);
+
       return Nullav;
     }
 
 process:
     imp_sth->currow++;
+
+    if (imp_sth->currow >= imp_sth->row_num)
+      DBIc_ACTIVE_off(imp_sth);
 
     av= DBIc_DBISTATE(imp_sth)->get_fbav(imp_sth);
     num_fields=mysql_stmt_field_count(imp_sth->stmt);
@@ -5232,8 +5219,8 @@ process:
                     SVfARG(sv_2mortal(my_ulonglong2sv(mysql_num_rows(imp_sth->result)))));
       PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\tmysql_affected_rows=%" SVf "\n",
                     SVfARG(sv_2mortal(my_ulonglong2sv(mysql_affected_rows(imp_dbh->pmysql)))));
-      PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\tmariadb_st_fetch for %p, currow= %d\n",
-                    sth,imp_sth->currow);
+      PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\tmariadb_st_fetch for %p, currow=%" SVf "\n",
+                    sth, SVfARG(sv_2mortal(my_ulonglong2sv(imp_sth->currow))));
     }
 
     if (!(cols= mysql_fetch_row(imp_sth->result)))
@@ -5246,8 +5233,13 @@ process:
         mariadb_dr_do_error(sth, mysql_errno(imp_dbh->pmysql),
                  mysql_error(imp_dbh->pmysql),
                  mysql_sqlstate(imp_dbh->pmysql));
+      if (!mysql_more_results(imp_dbh->pmysql))
+        DBIc_ACTIVE_off(imp_sth);
       return Nullav;
     }
+
+    if (imp_sth->currow >= imp_sth->row_num && !mysql_more_results(imp_dbh->pmysql))
+      DBIc_ACTIVE_off(imp_sth);
 
     num_fields= mysql_num_fields(imp_sth->result);
     fields= mysql_fetch_fields(imp_sth->result);
@@ -5380,32 +5372,22 @@ int mariadb_st_finish(SV* sth, imp_sth_t* imp_sth) {
   }
 
   if (imp_sth->use_server_side_prepare && imp_sth->stmt)
-  {
-    /*
-      We have to fetch all data from stmt
-      There is may be useful for 2 cases:
-      1. st_finish when we have undef statement
-      2. call st_execute again when we have some unfetched data in stmt
-     */
-    if (DBIc_ACTIVE(imp_sth) && mariadb_st_describe(sth, imp_sth) && !imp_sth->fetch_done)
-      mysql_stmt_free_result(imp_sth->stmt);
-  }
+    mysql_stmt_free_result(imp_sth->stmt);
+
+  /*
+    Clean-up previous result set(s) for sth to prevent
+    'Commands out of sync' error
+  */
+  if (!mariadb_st_free_result_sets(sth, imp_sth, FALSE))
+    return 0;
 
   /*
     Cancel further fetches from this cursor.
     We don't close the cursor till DESTROY.
     The application may re execute it.
   */
-  if (imp_sth && DBIc_ACTIVE(imp_sth))
-  {
-    /*
-      Clean-up previous result set(s) for sth to prevent
-      'Commands out of sync' error
-    */
-    if (!mariadb_st_free_result_sets(sth, imp_sth))
-      return 0;
-  }
   DBIc_ACTIVE_off(imp_sth);
+
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
   {
     PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\n<-- mariadb_st_finish\n");
@@ -5436,6 +5418,9 @@ void mariadb_st_destroy(SV *sth, imp_sth_t *imp_sth) {
   imp_sth_fbh_t *fbh;
   int num_params;
   int num_fields;
+
+  mariadb_st_finish(sth, imp_sth);
+  mariadb_st_free_result_sets(sth, imp_sth, TRUE);
 
   if (imp_sth->statement)
     Safefree(imp_sth->statement);
@@ -5472,8 +5457,6 @@ void mariadb_st_destroy(SV *sth, imp_sth_t *imp_sth) {
     mysql_stmt_close(imp_sth->stmt);
     imp_sth->stmt= NULL;
   }
-
-  /* mariadb_st_finish has already been called by .xs code if needed.	*/
 
   /* Free values allocated by mariadb_st_bind_ph */
   if (imp_sth->params)
@@ -6548,7 +6531,6 @@ my_ulonglong mariadb_db_async_result(SV* h, MYSQL_RES** resp)
           num_fields = mysql_num_fields(imp_sth->result);
           DBIc_NUM_FIELDS(imp_sth) = (num_fields <= INT_MAX) ? num_fields : INT_MAX;
           imp_sth->done_desc = FALSE;
-          imp_sth->fetch_done = FALSE;
         }
       }
       imp_sth->warning_count = mysql_warning_count(imp_dbh->pmysql);

dbdimp.h
@@ -553,8 +553,7 @@ struct imp_sth_st {
     bool disable_fallback_for_server_prepare;
 
     MYSQL_RES* result;       /* result                                 */
-    int currow;           /* number of current row                  */
-    bool fetch_done;      /* mark that fetch done                   */
+    my_ulonglong currow;  /* number of current row                  */
     my_ulonglong row_num;         /* total number of rows                   */
 
     bool  done_desc;      /* have we described this sth yet ?	    */